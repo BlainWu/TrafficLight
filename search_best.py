6b2a2488e7a7f2af910582521563383ecd2aec58
BlainWu <41843271+BlainWu@users.noreply.github.com>
2020-05-31 17:53:47 +0800

    update

--- search_best.py
+++ search_best.py
@@ -13,18 +13,25 @@ for i,data in enumerate(dir_list):
 epoch_list.sort(key = lambda x : int(x.split('_')[1]))
 del(epoch_list[-1])
 max_socre = 0.0
+score_list = []
 for i,data in enumerate(epoch_list):
+    buffer_data = []
     xml_path = os.path.join(data,model_xml)
     fd = open(xml_path,'r')
     fd = yaml.load(fd)
     score = float(fd['_Attributes']['eval_metrics']['bbox_map'])
+    buffer_data.append(data)
+    buffer_data.append(score)
+    score_list.append(buffer_data)
     if score>max_socre:
         max_socre = score
         epoch = data
     else:
         continue
-print("验证集最优结果为{0},出自{1}".format(max_socre,epoch))
-
-#fd = open("epoch_1/model.yml",'r')
-#fd = yaml.load(fd)
-#print(fd['_Attributes']['eval_metrics']['bbox_map'])
+score_list.sort(key = lambda x:float(x[1]),reverse = True)
+print('-------------------------------------------------------------')
+print("验证集最优结果为{0},出自{1}".format(score_list[0][1],score_list[0][0]))
+print('-------------------------------------------------------------')
+print('模型序号                 分数')
+for i,data in enumerate(score_list):
+    print("{0}                 {1}".format(data[0],data[1]))
